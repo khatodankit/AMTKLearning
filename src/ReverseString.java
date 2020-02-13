c53b27515be0f1154a7b189925487d97d41545a6
khatodankit <khatodankit@gmail.com>
2020-02-13 09:08:11 +0530

    Couple more reverseString methods

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,12 +1,31 @@
 public class ReverseString {
-
-    public String reverseString(String str) {
+    public String reverseStringBasic(String str) {
         char[] charArray = str.toCharArray();
         StringBuilder revStr = new StringBuilder();
         for (char c : charArray) {
             revStr.insert(0, c);
         }
-
         return revStr.toString();
+    }
+
+
+    public String reverseStringSingleVarSpace(String str) {
+        char[] charArray = str.toCharArray();
+        char tempVar;
+        int length = charArray.length;
+        for (int i = 0; i < (length / 2); i++) {
+            tempVar = charArray[length - i - 1];
+            charArray[length - i - 1] = charArray[i];
+            charArray[i] = tempVar;
+        }
+        return String.copyValueOf(charArray);
+
+    }
+
+    public String reverseStringRecursive(String str) {
+        if (str.length() <= 1)
+            return str;
+
+        return reverseStringRecursive(str.substring(1) + str.charAt(0));
     }
 }
--- tests/ReverseStringTest.java
+++ tests/ReverseStringTest.java
@@ -1,9 +1,13 @@
 import org.junit.Assert;
+import org.junit.Test;
 
 public class ReverseStringTest {
 
-    @org.junit.Test
+    @Test
     public void reverseString() {
-        Assert.assertEquals("abcd", new ReverseString().reverseString("dcba"));
+        Assert.assertEquals("abcd", new ReverseString().reverseStringBasic("dcba"));
+
+        Assert.assertEquals("abcd", new ReverseString().reverseStringSingleVarSpace("dcba"));
+        Assert.assertEquals("abcde", new ReverseString().reverseStringSingleVarSpace("edcba"));
     }
 }
