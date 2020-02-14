92a8a27c93b678176596d52782f6d020393cd3e5
khatodankit <khatodankit@gmail.com>
2020-02-14 09:13:25 +0530

    Additional test case

--- tests/FirstNonRepeatingTest.java
+++ tests/FirstNonRepeatingTest.java
@@ -8,6 +8,7 @@ public class FirstNonRepeatingTest {
         Assert.assertEquals(java.util.Optional.of('a').get(), new FirstNonRepeating().findFirstNonRepeating("ankit"));
         Assert.assertEquals(java.util.Optional.of('a').get(), new FirstNonRepeating().findFirstNonRepeating("bbddffrrakkbb"));
 
+        Assert.assertEquals(java.util.Optional.of('z').get(), new FirstNonRepeating().findFirstNonRepeating("aabbzhhjc"));
 
         Assert.assertNull(new FirstNonRepeating().findFirstNonRepeating("aabbcc"));
     }
