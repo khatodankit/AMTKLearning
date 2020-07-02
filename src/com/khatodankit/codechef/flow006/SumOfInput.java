7ba39eaf391181eb9a5054c9e36398da7cd7bd51
khatodankit <khatodankit@gmail.com>
2020-07-02 16:55:27 +0530

    Codechef flow006

---
+++ src/com/khatodankit/codechef/flow006/SumOfInput.java
@@ -0,0 +1,51 @@
+package com.khatodankit.codechef.flow006;
+
+import java.util.Scanner;
+
+public class SumOfInput {
+    public static void main(String[] args) throws java.lang.Exception {
+        int[] inputs = getUserInputs();
+        for (int i = 0; i < inputs.length; i++) {
+            System.out.println(sumOfDigits(inputs[i]));
+        }
+    }
+
+    /**
+     * Gets the user input
+     * The first line contains an integer T, the total number of testcases. Then follow T lines, each line contains an integer N.
+     *
+     * @return int array
+     */
+    private static int[] getUserInputs() {
+        Scanner sc = new Scanner(System.in);
+        int[] inputs = new int[0];
+        if (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            inputs = new int[a];
+            for (int i = 0; i < a; i++) {
+                inputs[i] = sc.nextInt();
+            }
+        }
+        return inputs;
+    }
+
+
+    /**
+     * returns sum of the digits
+     *
+     * @param input
+     * @return
+     */
+    private static int sumOfDigits(int input) {
+        int sum = 0;
+        int lastDigit;
+        while (input != 0) {
+            lastDigit = input % 10;
+            input -= lastDigit;
+            input /= 10;
+
+            sum += lastDigit;
+        }
+        return sum;
+    }
+}
